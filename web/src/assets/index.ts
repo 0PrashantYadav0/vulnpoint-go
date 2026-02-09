415b616e84560ea142eee6119f074b9a332f577f
Dishant-garg <dgarg9803@gmail.com>
2026-02-09 23:31:23 +0530

    Deleted everything

--- web/src/assets/index.ts
+++
@@ -1,5 +0,0 @@
-import vulnpilotLogo from "./vulnpilot-logo.png";
-import vulnpilotLogoLight from "./vulnpilot-logo-light.png";
-import authScreenLeftSectionBgImage from "./auth-left-section-bg-image.png";
-
-export { vulnpilotLogo, vulnpilotLogoLight, authScreenLeftSectionBgImage };
